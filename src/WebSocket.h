19118c85ed57ec1ce2bc79e931318c3386e1581f
OLFDB <olf@eisenzelt.de>
2019-07-22 18:25:20 +0200

    Update WebSocket.h

--- src/WebSocket.h
+++ src/WebSocket.h
@@ -19,6 +19,9 @@
 
 #include "Clients.h"
 
+#define HTONLL(x) ((1==htonl(1)) ? (x) : (((uint64_t)htonl((x) & 0xFFFFFFFFUL)) << 32) | htonl((uint32_t)((x) >> 32)))
+#define NTOHLL(x) ((1==ntohl(1)) ? (x) : (((uint64_t)ntohl((x) & 0xFFFFFFFFUL)) << 32) | ntohl((uint32_t)((x) >> 32)))
+
 /**
  * WebSocket op codes
  * @{
